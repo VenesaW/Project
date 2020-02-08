6e9e989f7fe355d431e816c85f621b23d0779f6c
VenesaW <33246635+VenesaW@users.noreply.github.com>
2020-02-08 11:50:54 +0100

    Add files via upload

--- Copy_Sw.c
+++ Copy_Sw.c
@@ -1643,10 +1643,8 @@ void handleMsg(u_char *Uselesspointr, const struct pcap_pkthdr *header, const u_
 		
 		//Retrieve MIC
 		printf("\n\nIncoming MIC: \n");
-		for (getData = 0; getData < 1; getData++)
-		{
-			printf("%02x", oldDigest[getData]);
-		}//endFOR
+		printf("%02x", oldDigest[0]);
+
 
 		//MAC generation
 		//Calculate hash and compare to appended hash
