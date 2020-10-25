cfbdc2e5db6c550dfa4f2d5acf94705c52788ccf
VenesaW <33246635+VenesaW@users.noreply.github.com>
2020-10-25 08:14:46 +0100

    Add files via upload

--- cTSNMIC_Sw.c
+++ cTSNMIC_Sw.c
@@ -2524,7 +2524,8 @@ void handleMsg(u_char *Uselesspointr, const struct pcap_pkthdr *header, const u_
 			//Calculate hash and compare to appended hash
 			counter = 0;
 			chaskeyMsgLen = 444;
-			chaskey(hash, plaintext, curr_ESSession_Key, chaskeySubkey1, chaskeySubkey2);//pointer to returned chasekey mac calculation
+			chaskey(hash, plaintext, ESSession_Key, chaskeySubkey1, chaskeySubkey2);
+			//chaskey(hash, plaintext, curr_ESSession_Key, chaskeySubkey1, chaskeySubkey2);//pointer to returned chasekey mac calculation
 			toggleBit[0] = incomingToggleBit[0];
 		}//endIF_ELSE
 
